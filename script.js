edc87b3737cbd57d9a63ca25bf31702ea4d1fc78
Jan Maciejewski <maciejewski.jan.m@gmail.com>
2025-02-06 15:51:39 +0100

    cleaned up functions, added board syncing

--- script.js
+++ script.js
@@ -14,43 +14,87 @@
 
 
 const board = document.querySelector(".board");
-const square = board.querySelectorAll("div")
+const row_UI = board.querySelectorAll(".row")
 
-const gameboard = function() {
-    let a1 = "";
-    let a2 = "";
-    let a3 = "";
-    let b1 = "";
-    let b2 = "";
-    let b3 = "";
-    let c1 = "";
-    let c2 = "";
-    let c3 = "";
-}();
+const gameboard = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+];
 
 const Player = function(mark) {
     const mark = mark;
     let score = 0;
     
     playerPick = function(pick) {
-        gameboard.pick = Player.mark;
+        if (gameboard[pick] == "") {
+            gameboard.pick = Player.mark;
+        };
     };
 };
 
 let x = new Player(x);
 let o = new Player(o);
+let nowMoving = x;
 
-const gamePlay = function(pick) {
-    let nowMoving = x;
+const checkWin = function() {
+        
+    //Check rows for win
+    for (let row of gameboard) {
+        if (row[0] === row[1] && 
+            row[0] === row[2] && 
+            row[0] !== "") {
+            return row[0];
+        };
+    };
+        
+    //Check columns for win
+    for (let col=0; col<3; col++) {
+        if (gameboard[0][col] === gameboard[1][col] &&
+            gameboard[0][col] === gameboard[2][col] &&
+            gameboard[0][col] !== ""
+        ) return gameboard[0][col];
+    };
 
-    if (gameboard(pick) == "") {
-        nowMoving.playerPick(pick)
-    }
-    
-    
+    // Check diagonals for a win
+    if (gameboard[0][0] === gameboard[1][1] &&
+        gameboard[0][0] === gameboard[2][2] &&
+        gameboard[0][0] !== ""
+    ) return gameboard[0][0];
+
+    if (gameboard[2][0] === gameboard[1][1] &&
+        gameboard[2][0] === gameboard[0][2] &&
+        gameboard[2][0] !== ""
+    ) return gameboard[2][0];
+
+}
+
+for (let row of gameboard) {
+    if (row.includes("")) {
+        return null
 };
 
+return "Draw";
+};
+
+const changePlayer = function() {
+    if (nowMoving === x) {
+        nowMoving = o;
+    } else {
+        nowMoving = x;
+    }
+};
 
 squareClick: new function() {
 
 }
+
+const syncBoard = function() {
+    for (let row = 0; row < 2; row++) {
+        for (let col = 0; col < 2; col++) {
+            let squareClass = `${String.fromCharCode(97 + row)}${col + 1}`
+            let squareElement = document.querySelector(`.${squareClass}`);
+            squareElement.innerHTML = gameboard[row][col];
+        }
+    }; 
+};
